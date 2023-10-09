9ebfd66638642601467548ead4762ae33b0e1e49
Alexander Skrock <alexanderskrock@gmx.de>
2023-10-10 00:18:21 +0200

    Initial transition to Grommet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { grommet, Grommet, Page, PageContent, PageHeader, Text } from 'grommet';
+import { deepMerge } from 'grommet/utils';
+
+const theme = deepMerge(grommet, {
+  global: {
+    colors: {
+      brand: '#228BE6',
+    },
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet theme={ theme } full>
+      <Page>
+        <PageContent>
+          <PageHeader title="Under construction!" />
+        </PageContent>
+      </Page>
+    </Grommet>
   );
 }
 
