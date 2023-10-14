6415148e088af1612db5065d74129f86aad9e14a
Alexander Skrock <alexanderskrock@gmx.de>
2023-10-14 17:38:20 +0200

    Fixed placeholder for projects page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import { Route, Routes } from "react-router-dom";
 
 import Root from "./Root"
 import MaintenancePage from "./maintenance/MaintenancePage";
-import { Projects } from 'grommet-icons';
 
 const App = () => (
   <Routes>
     <Route Component={ Root }>
-      <Route path="projects" Component={ Projects } />
+      <Route path="projects" Component={ MaintenancePage } />
       <Route path="knowledge" Component={ MaintenancePage } />
       <Route path="contact" Component={ MaintenancePage } />
       <Route index Component={ MaintenancePage } />
