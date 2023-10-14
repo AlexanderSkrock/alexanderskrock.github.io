f159ec9be4eff09094d02026bc4af06ce7f5cc20
Alexander Skrock <alexanderskrock@gmx.de>
2023-10-14 15:47:11 +0200

    Integrated memory router into tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom'
+
 import { render, screen } from '@testing-library/react';
+
 import App from './App';
 
 test('renders under construction info', () => {
-  render(<App />);
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  )
   const elements = screen.getAllByAltText(/maintenance/i);
   expect(elements).toHaveLength(1);
   expect(elements[0]).toBeInTheDocument();
