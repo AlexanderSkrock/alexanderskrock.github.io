2a3ba3d064f36f4623847e9df6fa780104e01d5a
Alexander Skrock <alexanderskrock@gmx.de>
2023-11-01 02:08:28 +0100

    Style contacts as card

--- src/contact/Contacts.js
+++ src/contact/Contacts.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { Grid } from "grommet";
+import { Card, CardBody, Grid } from "grommet";
 import { Github, Linkedin, Mail } from "grommet-icons";
 
 const Contacts = () => (
-    <Grid columns={ [ "xxsmall", "auto" ] } gap="small" align="center">
-        <Mail />
-        <Link to="mailto://alexander@skrock.dev">alexander@skrock.dev</Link>
-        <Linkedin />
-        <Link to="https://www.linkedin.com/in/alexander-skrock/">Alexander Skrock</Link>
-        <Github />
-        <Link to="https://github.com/AlexanderSkrock">AlexanderSkrock</Link>
-    </Grid>
+    <Card>
+        <CardBody>
+            <Grid columns={ [ "xxsmall", "auto" ] } gap="small" align="center">
+                <Mail />
+                <Link to="mailto://alexander@skrock.dev">alexander@skrock.dev</Link>
+                <Linkedin />
+                <Link to="https://www.linkedin.com/in/alexander-skrock/">Alexander Skrock</Link>
+                <Github />
+                <Link to="https://github.com/AlexanderSkrock">AlexanderSkrock</Link>
+            </Grid>
+        </CardBody>
+    </Card>
 );
 
 export default Contacts;
