21c2f3199a7c8e95a3cb23d3bd4c7885001ada98
Alexander Skrock <alexanderskrock@gmx.de>
2023-11-01 01:53:06 +0100

    Improved full view port pages

---
+++ src/ConstantsContext.js
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+const ConstantsContext = createContext({
+    mainHeight: "100vh",
+})
+
+export default ConstantsContext;
--- src/Root.js
+++ src/Root.js
@@ -1,25 +1,41 @@
-import React from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import { Outlet } from "react-router-dom";
 
 import { Anchor, Grommet, Header, Main, Nav } from 'grommet';
 import { Certificate, HomeRounded, Projects, Contact } from "grommet-icons";
 
+import ConstantsContext from "./ConstantsContext";
 import theme from "./theme";
 
-const Root = () => (
-    <Grommet theme={ theme }>
-        <Header sticky="scrollup">
-            <Nav direction="row" fill="horizontal" justify="around" pad="small" background="brand">
-                <Anchor href="/" icon={ <HomeRounded />} hoverIndicator />
-                <Anchor href="/projects" icon={ <Projects />} hoverIndicator />
-                <Anchor href="/knowledge" icon={ <Certificate /> } hoverIndicator />
-                <Anchor href="/contact" icon={ <Contact /> } hoverIndicator />
-            </Nav>
-        </Header>
-        <Main background="background">
-            <Outlet />
-        </Main>
-    </Grommet>
-);
+const Root = () => {
+    const [mainHeight, setMainHeight] = useState("100vh");
+    const constants = useMemo(() => ({
+        mainHeight,
+    }), [mainHeight]);
+
+    const consumeHeader = useCallback(headerRef => {
+        if (headerRef) {
+            setMainHeight(`calc(100vh - ${headerRef.clientHeight}px)`);
+        }
+    });
+
+    return (
+        <Grommet theme={ theme }>
+            <Header sticky="scrollup" ref={ consumeHeader }>
+                <Nav direction="row" fill="horizontal" justify="around" pad="small" background="brand">
+                    <Anchor href="/" icon={ <HomeRounded />} hoverIndicator />
+                    <Anchor href="/projects" icon={ <Projects />} hoverIndicator />
+                    <Anchor href="/knowledge" icon={ <Certificate /> } hoverIndicator />
+                    <Anchor href="/contact" icon={ <Contact /> } hoverIndicator />
+                </Nav>
+            </Header>     
+            <ConstantsContext.Provider value={ constants }>
+                <Main background="background">
+                        <Outlet />
+                </Main>
+            </ConstantsContext.Provider>
+        </Grommet>
+    );
+}
 
 export default Root;
