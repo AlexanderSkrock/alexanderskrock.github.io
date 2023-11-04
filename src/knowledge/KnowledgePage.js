851c09037cbee6734ce6a8d64da149eacd207428
Alexander Skrock <alexanderskrock@gmx.de>
2023-11-04 13:39:02 +0100

    Basic structure for the knowledge page

--- src/knowledge/KnowledgePage.js
+++ src/knowledge/KnowledgePage.js
@@ -1,9 +1,29 @@
 import React from "react";
-import { Page, PageContent } from "grommet";
+import { Card, CardHeader, Page, PageContent, Text } from "grommet";
 
 const KnowledgePage = () => (
     <Page data-testid="knowledgePage">
         <PageContent>
+            <Card>
+                <CardHeader pad="medium">
+                    <Text weight="bold">Languages</Text>
+                </CardHeader>
+            </Card>
+            <Card>
+                <CardHeader pad="medium">
+                    <Text weight="bold">Frameworks & Technologies</Text>
+                </CardHeader>
+            </Card>
+            <Card>
+                <CardHeader pad="medium">
+                    <Text weight="bold">Tools</Text>
+                </CardHeader>
+            </Card>
+            <Card>
+                <CardHeader pad="medium">
+                    <Text weight="bold">Trainings & Certificates</Text>
+                </CardHeader>
+            </Card>
         </PageContent>
     </Page>
 );
