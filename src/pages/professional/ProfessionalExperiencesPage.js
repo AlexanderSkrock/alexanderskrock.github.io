c704b72d8a15c8751581cfb5a180b317079e3754
Alexander Skrock <alexanderskrock@gmx.de>
2024-06-04 01:30:36 +0200

    chore(experiences): cleanup and fix linting

--- src/pages/professional/ProfessionalExperiencesPage.js
+++ src/pages/professional/ProfessionalExperiencesPage.js
@@ -1,24 +1,20 @@
-import React, { useContext } from "react";
-import { List, Page, PageContent, ResponsiveContext } from "grommet";
+import React from "react";
+import { List, Page, PageContent } from "grommet";
 
 
 import experiences from "../../config/generated/experiences";
 import ExperienceCard from "./ExperienceCard";
 
-const ProfessionalExperiencesPage = () => {
-    const size = useContext(ResponsiveContext);
-
-    return (
-        <Page data-testid="professionalExperiencesPage" kind="full">
-            <PageContent>
-                <List data={ experiences }>
-                    {
-                        (experience, index) => <ExperienceCard key={ `experience_${index}` } experience={ experience } />
-                    }
-                </List>
-            </PageContent>
-        </Page>
-    );
-}
+const ProfessionalExperiencesPage = () => (
+    <Page data-testid="professionalExperiencesPage" kind="full">
+        <PageContent>
+            <List data={ experiences }>
+                {
+                    (experience, index) => <ExperienceCard key={ `experience_${index}` } experience={ experience } />
+                }
+            </List>
+        </PageContent>
+    </Page>
+);
 
 export default ProfessionalExperiencesPage
